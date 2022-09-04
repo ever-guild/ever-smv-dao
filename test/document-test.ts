6f419be111817d43a6889ff94adeafcf5c447d3c
Ilyar <761285+ilyar@users.noreply.github.com>
2022-09-04 15:21:36 +0200

    test: add scenario for create chapter 🐌

--- test/document-test.ts
+++ test/document-test.ts
@@ -44,5 +44,15 @@ describe("Test base flow", async function () {
       response = await dao.methods.show_draft({}).call();
       expect(response.out).to.be.equal("\t");
     });
+
+    it("Create chapter", async function () {
+      let response = await dao.methods.show_all({}).call();
+      expect(response.out).to.be.equal("");
+      await dao.methods.add_line({ line: "foo" }).sendExternal({ publicKey: signer.publicKey });
+      await dao.methods.add_line({ line: "bar" }).sendExternal({ publicKey: signer.publicKey });
+      await dao.methods.add_chapter({ n: 0 }).sendExternal({ publicKey: signer.publicKey });
+      response = await dao.methods.show_all({}).call();
+      expect(response.out).to.be.equal("\n\tChapter 0\n\tfoo\nbar\n");
+    });
   });
 });
